df792b483e58e582c8f137a4de289e29eee46ab6
Thomas Siig Nørgaard <thsn@mercell.com>
2021-09-20 23:02:20 +0200

    Additional unit tests for different numbers of Three A's implementation

--- PromotionEngine.Tests/ThreeAFixedPricePromotionTests.cs
+++ PromotionEngine.Tests/ThreeAFixedPricePromotionTests.cs
@@ -30,5 +30,62 @@ namespace PromotionEngine.Tests
       promotion.Calculate(cart);
       Assert.AreEqual(130m, cart.TotalPrice);
     }
+
+    [Test]
+    public void Can_Give_Discount_On_Six_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "A",
+          Price = 50m,
+          Amount = 6
+        }
+      };
+
+      ThreeAFixedPricePromotion promotion = new ThreeAFixedPricePromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(260m, cart.TotalPrice);
+    }
+
+    [Test]
+    public void Can_Give_Discount_On_Five_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "A",
+          Price = 50m,
+          Amount = 4
+        }
+      };
+
+      ThreeAFixedPricePromotion promotion = new ThreeAFixedPricePromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(230m, cart.TotalPrice);
+    }
+
+    [Test]
+    public void No_Discount_On_Two_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "A",
+          Price = 50m,
+          Amount = 2
+        }
+      };
+
+      ThreeAFixedPricePromotion promotion = new ThreeAFixedPricePromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(100m, cart.TotalPrice);
+    }
   }
 }
