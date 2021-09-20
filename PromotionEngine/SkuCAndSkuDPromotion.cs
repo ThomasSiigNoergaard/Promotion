0fc721ff6a9e5dff77b731c943dad735739c00f3
Thomas Siig Nørgaard <thsn@mercell.com>
2021-09-20 22:55:05 +0200

    Implementation of SKU C and SKU D calculator

--- PromotionEngine/SkuCAndSkuDPromotion.cs
+++ PromotionEngine/SkuCAndSkuDPromotion.cs
@@ -13,7 +13,29 @@ namespace PromotionEngine
   {
     public void Calculate(Cart cart)
     {
-      throw new NotImplementedException();
+      var itemSkuC = cart.Items.FirstOrDefault(item => item.SKU.Equals("C"));
+      var itemSkuD = cart.Items.FirstOrDefault(item => item.SKU.Equals("D"));
+
+      if (itemSkuC?.Amount > 0 && itemSkuD?.Amount > 0)
+      {
+        if (itemSkuD.Amount > itemSkuC.Amount)
+        {
+          var numberOfPromotions = itemSkuC.Amount;
+          var remainingDItems = itemSkuD.Amount - itemSkuC.Amount;
+          cart.TotalPrice = (numberOfPromotions * 30) + (remainingDItems * 15);
+        }
+        else if(itemSkuD.Amount < itemSkuC.Amount)
+        {
+          var numberOfPromotions = itemSkuD.Amount;
+          var remainingDItems = itemSkuC.Amount - itemSkuD.Amount;
+          cart.TotalPrice = (numberOfPromotions * 30) + (remainingDItems * 20);
+        }
+        else
+        {
+          var numberOfPromotions = itemSkuC.Amount;
+          cart.TotalPrice = (numberOfPromotions * 30);
+        }
+      }
     }
   }
 }
