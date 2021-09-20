6954d1393176e1516574999c7b2ae2ef7463a1c8
Thomas Siig Nørgaard <thsn@mercell.com>
2021-09-20 22:42:52 +0200

    Unit Test for Sku C And D should give 30

---
+++ PromotionEngine.Tests/SkuCAndSkuDPromotionTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Tests
+{
+  using NUnit.Framework;
+  using PromotionEngine.Models;
+
+  class SkuCAndSkuDPromotionTests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Can_Give_Discount_On_One_C_And_One_D_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "C",
+          Price = 20m,
+          Amount = 1
+        },
+        new Item()
+        {
+          SKU = "D",
+          Price = 15,
+          Amount = 1
+        }
+      };
+
+      SkuCAndSkuDPromotion promotion = new SkuCAndSkuDPromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(30m, cart.TotalPrice);
+    }
+  }
+}
---
+++ PromotionEngine/SkuCAndSkuDPromotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+  using PromotionEngine.Interfaces;
+  using PromotionEngine.Models;
+
+  public class SkuCAndSkuDPromotion : IPromotion
+  {
+    public void Calculate(Cart cart)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
