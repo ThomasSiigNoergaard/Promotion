8ff9c577c113d45d7cd5f5b7ad7177a30811eeeb
Thomas Siig Nørgaard <thsn@mercell.com>
2021-09-20 22:59:34 +0200

    Added additional unit tests for sku c and sku d numbers

--- PromotionEngine.Tests/SkuCAndSkuDPromotionTests.cs
+++ PromotionEngine.Tests/SkuCAndSkuDPromotionTests.cs
@@ -40,5 +40,80 @@ namespace PromotionEngine.Tests
       promotion.Calculate(cart);
       Assert.AreEqual(30m, cart.TotalPrice);
     }
+
+    [Test]
+    public void Can_Give_Discount_On_Two_C_And_Two_D_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "C",
+          Price = 20m,
+          Amount = 2
+        },
+        new Item()
+        {
+          SKU = "D",
+          Price = 15,
+          Amount = 2
+        }
+      };
+
+      SkuCAndSkuDPromotion promotion = new SkuCAndSkuDPromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(60m, cart.TotalPrice);
+    }
+
+    [Test]
+    public void Can_Give_Discount_On_Two_C_And_One_D_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "C",
+          Price = 20m,
+          Amount = 2
+        },
+        new Item()
+        {
+          SKU = "D",
+          Price = 15,
+          Amount = 1
+        }
+      };
+
+      SkuCAndSkuDPromotion promotion = new SkuCAndSkuDPromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(50m, cart.TotalPrice);
+    }
+
+    [Test]
+    public void Can_Give_Discount_On_One_C_And_Two_D_Items()
+    {
+      Cart cart = new Cart();
+      cart.Items = new List<Item>()
+      {
+        new Item()
+        {
+          SKU = "C",
+          Price = 20m,
+          Amount = 1
+        },
+        new Item()
+        {
+          SKU = "D",
+          Price = 15,
+          Amount = 2
+        }
+      };
+
+      SkuCAndSkuDPromotion promotion = new SkuCAndSkuDPromotion();
+      promotion.Calculate(cart);
+      Assert.AreEqual(45m, cart.TotalPrice);
+    }
   }
 }
